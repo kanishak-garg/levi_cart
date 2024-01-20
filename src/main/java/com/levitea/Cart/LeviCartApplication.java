5f6808ca0c958d001ea456575efe1c0855cdeab8
kanishak garg <kanishakgarg8778@gmail.com>
2024-01-20 09:44:27 +0530

    intial setup

---
+++ src/main/java/com/levitea/Cart/LeviCartApplication.java
@@ -0,0 +1,13 @@
+package com.levitea.Cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeviCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeviCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levitea/Cart/LeviCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levitea.Cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeviCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
