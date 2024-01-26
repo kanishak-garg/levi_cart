455aae03dfe53443f19b57a71763dc7c8392a18c
kanishak garg <kanishakgarg8778@gmail.com>
2024-01-27 00:53:14 +0530

    write JPQL query

--- src/main/java/com/levitea/Cart/repository/ItemDetailsRepository.java
+++ src/main/java/com/levitea/Cart/repository/ItemDetailsRepository.java
@@ -1,6 +1,7 @@
 package com.levitea.Cart.repository;
 
 import com.levitea.Cart.entity.ItemDetails;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +9,7 @@ import java.util.List;
 public interface ItemDetailsRepository extends CrudRepository<ItemDetails,Integer> {
     //below method will not work in crudRepository, as crud does not support it. see next commit to see its application using jpaRepository
     List<ItemDetails> findByPriceGreaterThan(double price);
+
+    @Query("select itd from ItemDetails itd where itd.category=?1 ORDER BY itd.price DESC")
+    List<ItemDetails> findByCategoryOrderByPrice(String category);
 }
