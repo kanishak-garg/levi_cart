829d0d9e9ee81a57e7efe70d718715ac3fc30f57
kanishak garg <kanishakgarg8778@gmail.com>
2024-01-27 00:49:14 +0530

    write native query

--- src/main/java/com/levitea/Cart/repository/ItemRepository.java
+++ src/main/java/com/levitea/Cart/repository/ItemRepository.java
@@ -1,9 +1,16 @@
 package com.levitea.Cart.repository;
 
 import com.levitea.Cart.entity.Item;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends CrudRepository<Item,Integer> {
+
+    @Query(value = "SELECT * FROM item WHERE description LIKE :desc% LIMIT 4",nativeQuery = true)
+    List<Item> getItemByDesc(@Param("desc") String desc);
 }
